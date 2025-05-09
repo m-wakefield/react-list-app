6d330203a4dd650894956b416012591a649aa101
Monique Wakefield <monique_wakefield@yahoo.com>
2025-05-09 05:54:31 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import ToDoList from './Components/ToDoList';
-
+import Contact from './Components/Contact';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <ToDoList />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <nav>
+            <Link to="/">Home</Link> | <Link to="/contact">Contact</Link>
+          </nav>
+
+          <Routes>
+            <Route path="/" element={<ToDoList />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </header>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
