1d1bdcd4b5bc1d175791e25c01a7f42c5a576da9
Monique Wakefield <monique_wakefield@yahoo.com>
2025-05-13 15:01:21 -0500

    update

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './components/App.jsx';
-import Contact from './components/Contact.jsx';
+import App from './App.jsx';
+import Contact from './Components/Contact.jxs';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
