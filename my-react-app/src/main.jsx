54bb1ca5f2753bcdb42701325995aa58b7c858e6
Monique Wakefield <monique_wakefield@yahoo.com>
2025-05-13 15:17:11 -0500

    Update main.jsx

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,8 +1,10 @@
+import React from 'react'; //
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './Components/App.jsx';
 import Contact from './Components/Contact.jsx';
 import './index.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
