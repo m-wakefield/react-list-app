207b7366f308026b181ce08ef9d95eca213b592e
Monique Wakefield <monique_wakefield@yahoo.com>
2025-05-13 19:16:15 -0500

    Update vite.config.js

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Replace 'your-repo-name' with the GitHub repo name (exact match)
+// Replace this with your GitHub repo name:
 export default defineConfig({
   base: '/react-list-app/',
   plugins: [react()],
